ed04114ab13f4794bf69a25ef32efaa2eeb1ab89
frederickwittman <39687162+frederickwittman@users.noreply.github.com>
2019-09-13 21:18:38 -0600

    Update lab01_part1.js

--- lab01_part1.js
+++ lab01_part1.js
@@ -1,3 +1,8 @@
+// Fred Wittman
+// Lab 01
+// COSC 3020-01
+// 13 September 2019
+
 function insertionSort(arr) {
   for(var i = 1; i < arr.length; i++) {
     var val = arr[i];
@@ -29,24 +34,28 @@ function testInsertionSort () {
   arr1 = insertionSort(arr);
   for (var i = 0; i < arr.length - 1; i++){
     if(arr1[i] > arr[i + 1]){
-      "Not sorted correctly."
-      return false
+      console.log("Not sorted correctly.");
+      console.log(arr1)
+      return
     }
   }
+  console.log("Sorted correctly.")
   console.log(arr1)
-  return true
 }
+
 function testInsertionSortReverse () {
   arr = Array.from({length: 10}, () => Math.floor(Math.random() * 40));
   arr1 = insertionSortReverse(arr);
   for (var i = 0; i < arr.length - 1; i++){
     if(arr1[i] > arr[i + 1]){
       console.log("Not sorted correctly.")
-      return false
+      console.log(arr1)
+      return
     }
   }
+  console.log("Sorted correctly.")
   console.log(arr1);
-  return true
+  
 }
 
 testInsertionSort();
